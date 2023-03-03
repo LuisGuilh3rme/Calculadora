c99d9a8e689c74a7e3305b04c57639767d972614
Luis Guilherme <luislebert01@outlook.com>
2023-03-03 15:51:12 -0300

    Adicionado uma função para realização de operações

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,11 +1,51 @@
-﻿double n1, n2;
+﻿double num1, num2; 
+int opcao = 0;
 
-Console.WriteLine("Digite o primeiro número:");
-n1 = double.Parse(Console.ReadLine());
-Console.WriteLine("Digite o primeiro número:");
-n2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Informe o primeiro número:");
+num1 = double.Parse(Console.ReadLine());
 
-Console.WriteLine("{0} + {1} = {2}", n1, n2, n1 + n2);
-Console.WriteLine("{0} - {1} = {2}", n1, n2, n1 - n2);
-Console.WriteLine("{0} * {1} = {2}", n1, n2, n1 * n2);
-Console.WriteLine("{0} / {1} = {2}", n1, n2, n1 / n2);
+Console.WriteLine("Informe o segundo número:");
+num2 = double.Parse(Console.ReadLine());
+
+while (opcao != 5)
+{
+    Menu();
+}
+
+void Menu() {
+    Console.Clear();
+    Console.WriteLine("1 - Somar");
+    Console.WriteLine("2 - Subtrair");
+    Console.WriteLine("3 - Multiplicar");
+    Console.WriteLine("4 - Dividir");
+    Console.WriteLine("5 - Sair");
+    Console.WriteLine("Escolha uma opção: ");
+    opcao = int.Parse(Console.ReadLine());
+
+    switch (opcao)
+    {
+        default:
+            Console.WriteLine("Opção inválida");
+            Console.ReadKey();
+            break;
+        case 1: 
+            Console.WriteLine("{0} + {1} = {2}", num1, num2, num1 + num2);
+            Console.ReadKey();
+            break;
+        case 2:
+            Console.WriteLine("{0} - {1} = {2}", num1, num2, num1 - num2);
+            Console.ReadKey();
+            break;
+        case 3:
+            Console.WriteLine("{0} * {1} = {2}", num1, num2, num1 * num2);
+            Console.ReadKey();
+            break;
+        case 4:
+            Console.WriteLine("{0} / {1} = {2}", num1, num2, num1 / num2);
+            Console.ReadKey();
+            break;
+        case 5:
+            Console.WriteLine("Até mais!");
+            break;
+    }
+}
