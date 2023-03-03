66c027d63e68bcc66c79f91e6a982a9271a46207
Luis Guilherme <luislebert01@outlook.com>
2023-03-03 15:10:40 -0300

    Add project files.

---
+++ Calculadora/Program.cs
@@ -0,0 +1,11 @@
+﻿double n1, n2;
+
+Console.WriteLine("Digite o primeiro número:");
+n1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Digite o primeiro número:");
+n2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("{0} + {1} = {2}", n1, n2, n1 + n2);
+Console.WriteLine("{0} - {1} = {2}", n1, n2, n1 - n2);
+Console.WriteLine("{0} * {1} = {2}", n1, n2, n1 * n2);
+Console.WriteLine("{0} / {1} = {2}", n1, n2, n1 / n2);
