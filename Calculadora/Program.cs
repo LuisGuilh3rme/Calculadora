d73ed9398a8a2ba2eebe0cca1edcf51bfcf1baa3
Luis Guilherme <luislebert01@outlook.com>
2023-03-04 15:46:49 -0300

    Nova branch, resolução com funções/métodos

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -9,10 +9,41 @@ num2 = double.Parse(Console.ReadLine());
 
 while (opcao != 5)
 {
-    Menu();
+    opcao = Menu();
+    switch (opcao)
+    {
+        default:
+            Console.WriteLine("Opção inválida");
+            Continuar();
+            break;
+
+        case 1:
+            Console.WriteLine("{0} + {1} = {2}", num1, num2, Soma());
+            Continuar();
+            break;
+
+        case 2:
+            Console.WriteLine("{0} - {1} = {2}", num1, num2, Subtracao());
+            Continuar();
+            break;
+
+        case 3:
+            Console.WriteLine("{0} * {1} = {2}", num1, num2, Multiplicacao());
+            Continuar();
+            break;
+
+        case 4:
+            Console.WriteLine("{0} / {1} = {2}", num1, num2, Divisao());
+            Continuar();
+            break;
+
+        case 5:
+            Console.WriteLine("Até mais!");
+            break;
+    }
 }
 
-void Menu()
+int Menu()
 {
     Console.Clear();
     Console.WriteLine("1 - Somar");
@@ -21,37 +52,31 @@ void Menu()
     Console.WriteLine("4 - Dividir");
     Console.WriteLine("5 - Sair");
     Console.WriteLine("Escolha uma opção: ");
-    opcao = int.Parse(Console.ReadLine());
+    return int.Parse(Console.ReadLine());
+}
 
-    switch (opcao)
-    {
-        default:
-            Console.WriteLine("Opção inválida");
-            Console.ReadKey();
-            break;
+void Continuar()
+{
+    Console.WriteLine("Aperte qualquer tecla para continuar:");
+    Console.ReadKey();
+}
 
-        case 1:
-            Console.WriteLine("{0} + {1} = {2}", num1, num2, num1 + num2);
-            Console.ReadKey();
-            break;
+double Soma()
+{
+    return num1 + num2;
+}
 
-        case 2:
-            Console.WriteLine("{0} - {1} = {2}", num1, num2, num1 - num2);
-            Console.ReadKey();
-            break;
+double Subtracao()
+{
+    return num1 - num2;
+}
 
-        case 3:
-            Console.WriteLine("{0} * {1} = {2}", num1, num2, num1 * num2);
-            Console.ReadKey();
-            break;
+double Multiplicacao()
+{
+    return num1 * num2;
+}
 
-        case 4:
-            Console.WriteLine("{0} / {1} = {2}", num1, num2, num1 / num2);
-            Console.ReadKey();
-            break;
-
-        case 5:
-            Console.WriteLine("Até mais!");
-            break;
-    }
+double Divisao()
+{
+    return num1 / num2;
 }
